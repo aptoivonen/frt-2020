3696a650673811f5bc0c81c05f330744d6b881d9
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-18 18:07:21 +0300

    Make mini book list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import ReactDOM from "react-dom";
+import "./index.css";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>jogn doe</h2>;
-const Message = () => <p>this is message</p>;
-
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  const title = "Little Blue Truck's Halloween";
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_SX184_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>Alice Schertle, Jill McElmurry </h4>
+      <p>{}</p>
+    </article>
+  );
+};
+ReactDOM.render(<BookList />, document.getElementById("root"));
