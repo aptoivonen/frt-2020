a3b63e1e938ed3971696b25e30868ac70d91162e
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-18 16:13:06 +0300

    Create first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h4>This is my first component</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
